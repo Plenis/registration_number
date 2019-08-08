bdc2a6a95dac336ae092149250154136a9c2e194
Plenis <plenissino@gmail.com>
2019-08-08 16:28:25 +0200

    added regex and clear function for error messages

--- registration_number-dom.js
+++ registration_number-dom.js
@@ -8,36 +8,59 @@ var numberPlate = document.querySelector(".enteredRegNum");
 var dropdownElem = document.querySelector(".dropdown")
 var townsElem = document.querySelector(".towns") //CA
 
-
+var message = document.querySelector('.msg');
 var instance = RegistrationOpp();
+
+function clearError() {
+	setTimeout(function () {
+		message.innerHTML = "";
+	}, 2000);
+}
 
 function regDisplayBtn() {
     var plate = numberPlate.value;
-    instance.addReg(plate)
 
-displayReg(instance.getReg());
+    var regex = /^[a-z]{2}\s\d[-0-9\s]{1,7}$/;
+    var result = regex.test(plate);
+    console.log(result)
+        var thePlate = document.querySelector("input[name='towns']:checked");
+
+
+        if(result !== true){
+            clearError()
+            message.innerHTML = "Registration number not valid! "
+        }else{
+            instance.addReg(plate)
+            displayReg(instance.getReg());
+            // message.innerHTML = "Added successfully"
+
+        }
+    
+
+
+
 
 }
 
-function displayReg(regArry){
+function displayReg(regArry) {
     displayPlateElem.innerHTML = "";
- for (let index = 0; index < regArry.length; index++) {
-     const element = regArry[index];
-     createPlates(element)
- }
+    for (let index = 0; index < regArry.length; index++) {
+        const element = regArry[index];
+        createPlates(element)
+    }
 }
 
 
-function createPlates(reg){
+function createPlates(reg) {
     var number = document.createElement("li");
     number.textContent = reg;
     displayPlateElem.appendChild(number);
 }
 
-function townsFilter(){
-    var town =townsElem.value;
+function townsFilter() {
+    var town = townsElem.value;
     var filteredReg = instance.filter(town)
-  displayReg(filteredReg)
+    displayReg(filteredReg)
 }
 
 
